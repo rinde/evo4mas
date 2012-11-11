4c6a6ff10ae6393f25ce01c500183a97b1b5d511
rinde <Rinde.vanLon@cs.kuleuven.be>
2012-11-11 13:47:04 +0100

    updated gendreau evaluator

--- mas/src/main/java/rinde/evo4mas/mas/gendreau06/Gendreau06Evaluator.java
+++ mas/src/main/java/rinde/evo4mas/mas/gendreau06/Gendreau06Evaluator.java
@@ -4,10 +4,16 @@
 package rinde.evo4mas.mas.gendreau06;
 
 import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Maps.newHashMap;
 import static java.util.Collections.unmodifiableList;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import org.jppf.task.storage.DataProvider;
 
@@ -18,7 +24,6 @@ import rinde.evo4mas.evo.gp.GPProgramParser;
 import rinde.evo4mas.mas.common.ExperimentUtil;
 import rinde.evo4mas.mas.common.ResultDTO;
 import rinde.evo4mas.mas.common.TruckContext;
-import rinde.evo4mas.mas.fabrirecht.FRSimulationTask;
 import ec.EvolutionState;
 import ec.gp.GPTree;
 
@@ -34,12 +39,40 @@ public class Gendreau06Evaluator extends GPEvaluator<GSimulationTask, ResultDTO,
 	private final List<String> testSet;
 	private final int numScenariosPerGeneration;
 	private final int numScenariosAtLastGeneration;
+	private final Map<String, String> scenarioCache;
+	private final int numVehicles;
 
 	public Gendreau06Evaluator() {
 		testSet = unmodifiableList(folds.get(0));
 		trainSet = unmodifiableList(ExperimentUtil.createTrainSet(folds, 0));
 		numScenariosAtLastGeneration = 5;
 		numScenariosPerGeneration = 3;
+
+		// TODO set proper nr of vehicles
+		numVehicles = 5;
+		scenarioCache = newHashMap();
+		try {
+			for (final String s : testSet) {
+				scenarioCache.put(s, readScenarioFile(s));
+			}
+			for (final String s : trainSet) {
+				scenarioCache.put(s, readScenarioFile(s));
+			}
+		} catch (final FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (final IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private String readScenarioFile(String file) throws IOException {
+		final StringBuilder sb = new StringBuilder();
+		final BufferedReader bf = new BufferedReader(new FileReader(file));
+		String line;
+		while ((line = bf.readLine()) != null) {
+			sb.append(line);
+		}
+		return sb.toString();
 
 	}
 
@@ -59,7 +92,7 @@ public class Gendreau06Evaluator extends GPEvaluator<GSimulationTask, ResultDTO,
 		final GPProgram<TruckContext> heuristic = GPProgramParser
 				.convertToGPProgram((GPBaseNode<TruckContext>) trees[0].child);
 
-		final List<FRSimulationTask> list = newArrayList();
+		final List<GSimulationTask> list = newArrayList();
 		final List<String> scenarios = getCurrentScenarios(state);
 		for (final String s : scenarios) {
 			try {
@@ -67,13 +100,9 @@ public class Gendreau06Evaluator extends GPEvaluator<GSimulationTask, ResultDTO,
 			} catch (final Exception e) {
 				throw new RuntimeException(e);
 			}
-			list.add(new FRSimulationTask(s, heuristic.clone(), numVehicles, vehicleCapacity));
+			list.add(new GSimulationTask(s, heuristic.clone(), numVehicles));
 		}
 		return list;
-		// final GPProgram<FRContext> acceptance = new
-		// GPProgram<FRContext>((GPFunc<FRContext>) trees[1].child);
-		// return asList(new FRSimulationDTO(heuristic,
-		// "files/scenarios/fabri-recht/pdp100_mitAnrufzeit/lc107.scenario"));
 	}
 
 	@Override
