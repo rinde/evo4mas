824ba47c71e11b13ef7eea199606f277609e85d7
rinde <Rinde.vanLon@cs.kuleuven.be>
2013-08-06 13:29:13 +0200

    added code to write loads to disk

--- src/test/java/rinde/dynscale/ScenarioGeneratorTest.java
+++ src/test/java/rinde/dynscale/ScenarioGeneratorTest.java
@@ -3,11 +3,18 @@
  */
 package rinde.dynscale;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import org.apache.commons.math3.random.MersenneTwister;
 import org.apache.commons.math3.random.RandomGenerator;
 import org.junit.Test;
 
 import rinde.sim.scenario.Scenario;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
 
 /**
  * @author Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
@@ -18,25 +25,42 @@ public class ScenarioGeneratorTest {
     @Test
     public void test() {
         final ScenarioGenerator sg = ScenarioGenerator.builder()
-                .setAnnouncementIntensityPerKm2(1d)
+                .setAnnouncementIntensityPerKm2(1.3d)
                 .setOrdersPerAnnouncement(1.3) //
                 .setScale(.1, 5) //
-                .setScenarioLength(120) //
+                .setScenarioLength(240) //
                 .build();
         final RandomGenerator rng = new MersenneTwister(123);
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10; i++) {
             final Scenario s = sg.generate(rng);
 
             Metrics.checkTimeWindowStrictness(s);
 
+            final List<Double> loads = Metrics.measureLoad(s);
+            writeLoads(loads, new File("files/generator/load/scenario" + i
+                    + ".load"));
+
             // measure dynamism
             // measure load
-
-            System.out.println(Metrics.measureLoad(s));
 
             System.out.println(s.size() + " " + Metrics.measureDynamism(s));
         }
 
+    }
+
+    static void writeLoads(List<Double> loads, File f) {
+        final StringBuilder sb = new StringBuilder();
+        int i = 0;
+        for (; i < loads.size(); i++) {
+            sb.append(i).append(" ").append(loads.get(i)).append("\n");
+        }
+        sb.append(i).append(" ").append(0).append("\n");
+        try {
+            Files.createParentDirs(f);
+            Files.write(sb.toString(), f, Charsets.UTF_8);
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
