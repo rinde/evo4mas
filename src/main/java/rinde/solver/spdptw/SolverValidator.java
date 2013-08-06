c0b4fa2664417b53ab2c84673743f2157c577593
rinde <Rinde.vanLon@cs.kuleuven.be>
2013-08-06 18:34:25 +0200

    removed some print statements

--- src/main/java/rinde/solver/spdptw/SolverValidator.java
+++ src/main/java/rinde/solver/spdptw/SolverValidator.java
@@ -142,17 +142,14 @@ public final class SolverValidator implements Solver {
         // checks for completeness of tour
         checkArgument(routeSet.equals(locationSet), "Not all locations are serviced, there is probably a non-existing location in the route.");
 
-        // TODO test this
         // check service pairs ordering, pickups shoud be visited before their
         // corresponding delivery location
         final Map<Integer, Integer> pairs = newHashMap();
         for (int i = 0; i < servicePairs.length; i++) {
             pairs.put(servicePairs[i][0], servicePairs[i][1]);
         }
-        System.out.println(pairs);
         final Set<Integer> seen = newHashSet();
         for (int i = 1; i < n - 1; i++) {
-            System.out.println(sol.route[i]);
             if (pairs.containsKey(sol.route[i])) {
                 checkArgument(!seen.contains(pairs.get(sol.route[i])), "Pickups should be visited before their corresponding deliveries. Location %s should be visited after location %s.", pairs
                         .get(sol.route[i]), sol.route[i]);
