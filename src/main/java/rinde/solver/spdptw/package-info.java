307260886fb0ec7a64573c544e9745fd19306e3f
rinde <Rinde.vanLon@cs.kuleuven.be>
2013-08-07 15:41:59 +0200

    removed spdptw package

--- src/main/java/rinde/solver/spdptw/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Contains a solver interface for the single vehicle pickup-and-delivery
- * problem with time windows (SPDPTW).
- */
-@ParametersAreNonnullByDefault
-package rinde.solver.spdptw;
-
-import javax.annotation.ParametersAreNonnullByDefault;
-
